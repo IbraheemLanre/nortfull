9e7da43fd46badfc6d7dfaea46cb5bda5412edbd
IbraheemLanre <s2001108@edu.bc.fi>
2021-12-29 22:34:46 +0200

    initial commit

---
+++ src/main/java/com/nortal/mega/persistence/BuildingRepository.java
@@ -0,0 +1,17 @@
+package com.nortal.mega.persistence;
+
+import com.nortal.mega.persistence.entity.BuildingDbo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BuildingRepository extends CrudRepository<BuildingDbo, Long> {
+
+    List<BuildingDbo> findAll();
+    BuildingDbo findBuildingById(Long id);
+    BuildingDbo save(BuildingDbo buildingDbo);
+    void deleteById(Long id);
+    void update(BuildingDbo buildingDbo);
+}
